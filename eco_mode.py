9a0491724793d81595339699decb6875d914849d
LutherCeltors <lutherceltors@gmail.com>
2024-11-14 15:20:48 +0700

    Update eco_mode.py

--- eco_mode.py
+++ eco_mode.py
@@ -4,11 +4,12 @@ eco_mode.lower()
 ulang = True
 while ulang :
     if eco_mode == "ya" :
+        print("ECO MODE AKTIF")
         print("Saat ini anda menggunakan 50% dari daya pada microwave.")
         ulang = False
     elif eco_mode == "tidak" :
         print("Saat in anda menggunakan 100% dari daya pada microwave.")
-        ulang = True
+        ulang = False
     else :
         print("Masukkkan pilihan dengan benar, ketik antara 'ya' atau 'tidak'")
         eco_mode = input("Apakah anda ingin mengaktifkan Eco Mode ? (ya/tidak) : ")
